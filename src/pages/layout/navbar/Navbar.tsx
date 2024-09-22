cc929b55e38c08f4769026cc704a4b9282e5a4cd
Binyam Tafesse <binyamtafesse@Binyams-MacBook-Pro-4.local>
2024-09-21 22:00:56 -0500

    more

--- src/pages/layout/navbar/Navbar.tsx
+++ src/pages/layout/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import {Flex,Box} from "@chakra-ui/react"
 const Navbar = () => {
   return (
     <Flex>
-     <Box>  This is Top Header</Box>
+     <Box>  This is Top Header the most topp Sir</Box>
     </Flex>
   )
 }
