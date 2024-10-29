90095126872338d1b9a6e4a822d6d6697e4363c7
Binyam Tafesse <binyamtafesse@Binyams-MacBook-Pro-4.local>
2024-10-28 23:41:57 -0500

    updated thebfooter

--- src/pages/layout/Footer.tsx
+++ src/pages/layout/Footer.tsx
@@ -1,7 +1,12 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Box } from "@chakra-ui/react";
 
 const Footer = () => {
-  return <Flex>This Is the Footer from TH efooter </Flex>;
+  return (
+    <Flex>
+      This Is the Footer from TH efooter
+      <Box>thei is the facebook logo</Box>
+    </Flex>
+  );
 };
 
 export default Footer;
